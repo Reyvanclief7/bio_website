bdaf64f9eb268a4d7273d0945bef9827d88485dc
Reyvan clief <rey17van17@gmail.com>
2024-11-17 13:49:03 +0800

    convert about to firebase

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,18 +1,26 @@
-const about = () => {
+import { getDatabase, ref, onValue } from "firebase/database";
+import React, { useEffect, useState } from "react";
+
+const About = () => {
+  const [about, setAbout] = useState([]);
+  useEffect(() => {
+    const db = getDatabase();
+    const aboutRef = ref(db, "about");
+    onValue(aboutRef, (snapshot) => {
+      const data = snapshot.val();
+      setAbout(data);
+    });
+  }, []);
   return (
     <section id="about" className="section">
-    <div className="container">
-      <h2>Tentang Saya</h2>
-      <p>
-        Saya Reyvan Kalangi, mahasiswa semester 5 jurusan Teknologi
-        Informasi di Universitas Klabat. Saya memiliki minat besar dalam
-        pengembangan *website* dan *game*, serta senang mengerjakan proyek
-        yang melibatkan pemrograman dan desain. Saat ini saya terus belajar
-        untuk memperdalam kemampuan teknis dalam dua bidang tersebut.
-      </p>
-    </div>
-  </section>
-  )
-}
+      <div className="container">
+        <h2>{about.title}</h2>
+        <p>
+          {about.subtitle}
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default about;
+export default About;
